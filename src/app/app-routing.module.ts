17594999bc4a868e7afd45571d592313c705bf4c
Linahermassi <117922846+Linahermassi@users.noreply.github.com>
2023-04-20 05:21:19 +0100

    intégration des templates

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { MenuComponent } from './Components/menu/menu.component';
+import { ContactComponent } from './Components/contact/contact.component';
+
+const routes: Routes = [
+  {path:'homepage',component:HomePageComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'',redirectTo:'homepage',pathMatch:'full'},
+  {path:'**',component:NotFoundComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './Components/menu/menu.component';
+import { ContractComponent } from './Components/contract/contract.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    ContractComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    ContactComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
