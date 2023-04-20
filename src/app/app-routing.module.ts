5f6af392340c03694e4c3d1a130d4e758ca92412
Linahermassi <117922846+Linahermassi@users.noreply.github.com>
2023-04-20 22:49:57 +0100

    gestion contrat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { MenuComponent } from './Components/menu/menu.component';
 import { ContactComponent } from './Components/contact/contact.component';
+import { AddContractComponent } from './Components/add-contract/add-contract.component';
+import { ContractComponent } from './Components/contract/contract.component';
 
 const routes: Routes = [
   {path:'homepage',component:HomePageComponent},
-  {path:'menu',component:MenuComponent},
+  {path:'menu',component:MenuComponent,children :
+  [{path:'contract',component:ContractComponent},
+  {path:'addcontract',component:AddContractComponent},]},
   {path:'contact',component:ContactComponent},
   {path:'',redirectTo:'homepage',pathMatch:'full'},
   {path:'**',component:NotFoundComponent}
